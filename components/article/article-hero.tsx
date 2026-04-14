8dce2ca772cffe09694e252ac6262b69f54cb3b2
Axel Verhaeghe <verhaeghe.a.j@gmail.com>
2026-04-14 21:54:01 +0200

    chore: add version replacing in cicd

--- components/article/article-hero.tsx
+++ components/article/article-hero.tsx
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   fallback: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFill,
     alignItems: "center",
     justifyContent: "center",
   },
